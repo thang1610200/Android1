1a997463f49fe2dfdf367f8dc1f861c21d274aaf
Your Name <20110568@student.hcmute.edu.vn>
2023-02-17 14:19:24 +0700

    update

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,31 +13,31 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.lab2_event);
+        setContentView(R.layout.change_language);
 
-        Button buttonkq = (Button) findViewById(R.id.btn);
-        buttonkq.setOnClickListener((View v) -> {
-                EditText edit_a = (EditText)findViewById(R.id.edit_a);
-                EditText edit_b = (EditText)findViewById(R.id.edit_b);
-                TextView kq = (TextView) findViewById(R.id.calculate_kq);
-                float a = Float.parseFloat(edit_a.getText().toString());
-                float b = Float.parseFloat(edit_b.getText().toString());
-
-                kq.setText(Round(a,b));
-        });
+//        Button buttonkq = (Button) findViewById(R.id.btn);
+//        buttonkq.setOnClickListener((View v) -> {
+//                EditText edit_a = (EditText)findViewById(R.id.edit_a);
+//                EditText edit_b = (EditText)findViewById(R.id.edit_b);
+//                TextView kq = (TextView) findViewById(R.id.calculate_kq);
+//                float a = Float.parseFloat(edit_a.getText().toString());
+//                float b = Float.parseFloat(edit_b.getText().toString());
+//
+//                kq.setText(Round(a,b));
+//        });
     }
 
-    private float Cong(float a,float b){
-        return a + b;
-    }
-
-    private String Round(float a,float b){
-        float kq = Cong(a,b);
-        String temp = String.valueOf(kq);
-        String save = temp;
-        if(kq - (int)kq == 0){
-            save = temp.substring(0,temp.length() - 2);
-        }
-        return save;
-    }
+//    private float Cong(float a,float b){
+//        return a + b;
+//    }
+//
+//    private String Round(float a,float b){
+//        float kq = Cong(a,b);
+//        String temp = String.valueOf(kq);
+//        String save = temp;
+//        if(kq - (int)kq == 0){
+//            save = temp.substring(0,temp.length() - 2);
+//        }
+//        return save;
+//    }
 }
